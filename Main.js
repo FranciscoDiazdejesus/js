c45b0a890d665ba27ea675fa3b76752ce455a3c0
FranciscoDiazdejesus <franciscojavier2882@gmail.com>
2024-09-03 20:24:34 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,18 @@
+// Declarar la variable
+const nu1 = prompt("Ingresa el numero 1")
+const nu2 = prompt("Ingresa el primer 2")
+const nu3 = prompt("Ingresa el primer 3")
+
+// convertir a enterp
+const nu1Int = Number(nu1)
+const nu2Int = Number(nu2)
+const nu3Int = Number(nu3)
+
+// sumar los numero
+const resultado = nu1Int + nu2Int + nu3Int
+
+// calcular primedio
+const promedio = resultado / 3
+
+// mostrar el resultado
+alert("Promedio de los tres numero es: " + promedio)
